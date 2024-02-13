5b773eb500807b0807ffbea1c83a7972217955da
dai <sugerbig15@gmail.com>
2024-02-13 23:26:12 +0900

    5回目の課題提出です

--- app/models/italian_food.rb
+++ app/models/italian_food.rb
@@ -1,2 +1,4 @@
 class ItalianFood < Food
+  has_many :order_foods, through: :orders
+  has_many :orders, through: :order_foods
 end
--- app/models/japanese_food.rb
+++ app/models/japanese_food.rb
@@ -1,2 +1,4 @@
 class JapaneseFood < Food
+  has_many :order_foods, through: :orders
+  has_many :orders, through: :order_foods
 end
